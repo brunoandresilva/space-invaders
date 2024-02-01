c5bdfd3dc3e744b091e1c312c76fb8f47ce602a7
xB0SsZz <a93095@alunos.uminho.pt>
2024-02-01 23:21:39 +0000

    moving ship (which is just a square)

--- space-invaders.py
+++ space-invaders.py
@@ -3,9 +3,11 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((720, 900))
 clock = pygame.time.Clock()
 running = True
+x_pos = 335
+y_pos = 800
 
 while running:
     # poll for events
@@ -15,9 +17,21 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
     # RENDER YOUR GAME HERE
+    ship = pygame.Rect(x_pos, y_pos, 50, 50)
+    pygame.draw.rect(screen, (36, 14, 235), ship)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and y_pos > 0:
+        y_pos -= 15 
+    if keys[pygame.K_s] and y_pos < 850:
+        y_pos += 15
+    if keys[pygame.K_a] and x_pos > 25:
+        x_pos -= 15 
+    if keys[pygame.K_d] and x_pos < 645:
+        x_pos += 15
 
     # flip() the display to put your work on screen
     pygame.display.flip()
